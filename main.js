182e32afc3fe7c72273a2bb0cf493627d47e6c40
TremalJ <>
2021-02-03 17:19:24 +0100

    Renderizar datos de forma declarativa

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+
+        return {
+            titulo: 'Este es un hola mundo con VUEJS',
+            cantidad: 500,
+            // cantidad: 'htttp://youtube.com',
+        }
+    }
+})
