5d9a59f0e3e4fe44bfb287eeaebf9eb1478ed06c
TremalJ <>
2021-02-04 10:53:09 +0100

    props. pasar datos entre componentes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 app.component('footer-banco', { //No se recomienda usar camelCase
+    data: ['cantidad', 'fecha'],
     template: /*html*/ `
         <div class="bg-dark py-3 mt-2 text-white">
-            <h3>{{texto}} - {{cantidad}}</h3>
+            <h3>{{texto}}</h3>
+            <p>{{fecha}}</p>
         </div>
     `,
     data() {
         return {
-            texto: 'Footer de mi sitio web dinámico',
-            cantidad: 1000
+            texto: 'Footer de mi sitio web dinámico'
         }
     }
 })
