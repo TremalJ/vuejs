2581a738a417d8bad2616b76279409d0f16a5911
TremalJ <>
2021-02-05 09:33:24 +0100

    fetch. consumir API

--- CLI/router-udemy-1/src/views/Blog.vue
+++ CLI/router-udemy-1/src/views/Blog.vue
@@ -1,5 +1,6 @@
 <template>
   <Titulo texto="Título de mi blog" />
+  <button @click="consumirApi">Consumir API</button>
 </template>
 
 <script>
@@ -7,6 +8,22 @@ import Titulo from '../components/Titulo.vue'
 
 export default {
   components: { Titulo },
+  data() {
+    return {
+      arrayBlog: []
+    }
+  },
+  methods: {
+    async consumirApi() {
+      try{
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const array = await data.json();
+        console.log(array);
+      } catch(error){
+        console.log(error);
+      }
+    }
+  }
 }
 </script>
 
