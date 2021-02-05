59f69f5712985038131189cdc80322c857c13d6b
TremalJ <>
2021-02-05 12:44:32 +0100

    State. principios

---
+++ CLI/vuex-1/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: { // objeto único que contiene el estado de aplicación.
+    contador: 50,
+  },
+  mutations: { //
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
