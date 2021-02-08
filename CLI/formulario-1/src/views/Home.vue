aac82e501b5bbff4d5d7cbc51c9bf5d856cd3b95
TremalJ <>
2021-02-08 08:53:47 +0100

    v-model. text

---
+++ CLI/formulario-1/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <form action="">
+    <input 
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese Nombre"
+      v-model="texto"
+    >
+    <p> 
+      {{texto.tarea}}
+    </p>
+  </form>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      tarea: {
+        texto:  ''
+      }
+    }
+  }
+}
+</script>
