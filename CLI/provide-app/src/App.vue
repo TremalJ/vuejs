17f07558b966aa7d8f849eda5bc60edf1e2803c9
TremalJ <>
2021-02-11 17:46:54 +0100

    practicando con provide e inject

---
+++ CLI/provide-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>App contador: {{contador}}</h1>
+  <ContadorUno />
+  <ContadorAccion />
+  <contador-accion></contador-accion>
+</template>
+
+<script>
+import { provide, ref } from 'vue';
+import ContadorUno from './components/ContadorUno'
+import ContadorAccion from './components/ContadorAccion'
+
+export default {
+  name: 'App',
+  components: {
+    ContadorUno,
+    ContadorAccion
+  },
+  setup(){
+    const contador = ref(0);
+
+    provide('contador', contador)
+
+    return {contador}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ CLI/provide-app/src/components/ContadorAccion.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="contador ++">Aumentar</button>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+    setup(){
+        const contador = inject('contador');
+
+        return {contador}
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
