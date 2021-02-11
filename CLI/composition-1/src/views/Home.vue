700f1e446bf33d120b38272fe533ffac45754061
TremalJ <>
2021-02-11 09:00:49 +0100

    setup, refs y metodos de composition

---
+++ CLI/composition-1/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Contador {{ contador }}</h1>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {
+    aumentar() {
+      this.contador = this.contador + 1;
+    },
+    disminuir() {
+      this.contador = this.contador - 1;
+    }
+  }
+};
+</script>
