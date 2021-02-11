70c90a4a56ca1066b925d3f2ff0ad29841411cfb
TremalJ <>
2021-02-11 12:02:23 +0100

    Hooks.reuso de codigo

--- CLI/composition-1/src/views/Paises.vue
+++ CLI/composition-1/src/views/Paises.vue
@@ -1,38 +1,15 @@
 <template>
   <h1>Lista de Paises</h1>
   <p v-for="(pais, index) in arrayPaises" :key="index">
-    {{ pais.name }}
+    <router-link :to="`/pais/${pais.name}`"> {{ pais.name }}</router-link>
   </p>
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
+import { useFetch } from "../hooks/useFetch";
 export default {
   setup() {
-    const arrayPaises = ref([]);
-
-    onMounted(async () => {
-      try {
-        const res = await fetch("api.json");
-        //const res = await fetch("https://restcountries.eu/rest/v2/all");
-        arrayPaises.value = await res.json();
-      } catch (error) {
-        console.log(error);
-      }
-    });
-
-    // const fetchData = async () => {
-    //   try {
-    //     const res = await fetch("api.json");
-    //     //const res = await fetch("https://restcountries.eu/rest/v2/all");
-    //     arrayPaises.value = await res.json();
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
-    // fetchData();
-
-    return { arrayPaises };
+    return { ...useFetch("api.json") };
   }
 };
 </script>
