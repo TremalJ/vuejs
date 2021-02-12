40ed79789da12d8b12ad124cf269675b8acf0a0a
TremalJ <>
2021-02-12 11:21:39 +0100

    Creador tareas. Pintar tareas con los props

--- CLI/tarea-provide/src/components/TareaApp.vue
+++ CLI/tarea-provide/src/components/TareaApp.vue
@@ -1,15 +1,26 @@
 <template>
     <h1 >App Tareas</h1>
     <tarea-form></tarea-form>
+    <tarea-item
+    v-for="tarea in tareas" :key="tarea.id" 
+    :tarea=tarea
+    >
+    </tarea-item>
+
+    <div class="alert alert-dark mt-3" v-if="tareas.length === 0">
+        Sin tareas pendientes
+    </div>
     <p>{{tareas}}</p>
 </template>
 
 <script>
 import { provide, ref } from 'vue';
 import TareaForm from './TareaForm';
+import TareaItem from './TareaItem';
 export default {
     components:{
-        TareaForm
+        TareaForm,
+        TareaItem
     },
     setup() {
         const tareas = ref([]);
---
+++ CLI/tarea-provide/src/components/TareaItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="alert alert-warning mt-3 d-flex justify-content-between align-items-center">
+      <p class="m-0">{{tarea.texto}}</p>
+      <div>
+          <i class="fas fa-check-circle mx-2 text-success" role="button"></i>
+          <i class="fas fa-minus-circle mx-2 text-danger" role="button"></i>
+          <i></i>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        tarea: Object,
+        required: false,
+    }
+}
+</script>
+
+<style>
+
+</style>
