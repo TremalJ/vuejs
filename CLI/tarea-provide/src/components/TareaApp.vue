d58ceef946225d5b58f23132c37a4527202f7a13
TremalJ <>
2021-02-12 10:56:39 +0100

    Creador tareas. provide e inject, en formularios

---
+++ CLI/tarea-provide/src/components/TareaApp.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 >App Tareas</h1>
+    <tarea-form></tarea-form>
+    <p>{{tareas}}</p>
+</template>
+
+<script>
+import { provide, ref } from 'vue';
+import TareaForm from './TareaForm';
+export default {
+    components:{
+        TareaForm
+    },
+    setup() {
+        const tareas = ref([]);
+
+        provide('tareas', tareas);
+
+        return { tareas }
+    }
+}
+</script>
+
+<style>
+
+</style>
