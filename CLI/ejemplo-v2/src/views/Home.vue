f1ee45bda31419a6e7bde59c06b49681c86ecdb9
TremalJ <>
2021-02-15 10:19:10 +0100

    crud con vue router y vuex

---
+++ CLI/ejemplo-v2/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Inicio />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Inicio from './Inicio'
+export default {
+  components:{
+    Inicio
+  },
+  name: 'Home',
+}
+</script>
