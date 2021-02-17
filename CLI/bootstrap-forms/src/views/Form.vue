07c369b25f68f00ce6d0bce4022c3da3f7f79d6c
TremalJ <>
2021-02-17 08:34:22 +0100

    BootstrapVue. form input select, radio y checkbox

--- CLI/bootstrap-forms/src/views/Form.vue
+++ CLI/bootstrap-forms/src/views/Form.vue
@@ -3,12 +3,31 @@
       <h1>Formularios</h1>
 
       <form>
-          <b-col>
+          <b-row>
+            <b-col md="4">
              <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
              <small>Mínimo 3 caracteres.</small>
-          </b-col>
+             <p> texto: {{texto}}</p>
+            </b-col>
+            <b-col md="4">
+                <b-form-select v-model="select" :options="animales"></b-form-select>
+                <p>Seleccion {{ select }}</p>
+            </b-col>
+            <b-col md="4" class="mt-3">
+                <b-form-radio-group v-model="radio" :options="animalesRadio">
+                <p>Seleccion {{ radio }}</p>
+                </b-form-radio-group>
+            </b-col>
+            <b-col md="4">
+                <b-form-checkbox-group 
+                v-model="selected" 
+                :options="animalesRadio">
+
+                </b-form-checkbox-group>
+                <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+            </b-col>
+          </b-row>
       </form>
-      <p> texto: {{texto}}</p>
   </div>
 </template>
 
@@ -17,12 +36,26 @@ export default {
     name: 'Form',
     data(){
         return{
-            texto: ''
+            texto: '',
+            select: null,
+            radio: null,
+            animales:[
+                {value: null, text: 'Seleccione un animal'},
+                {value: 'perro', text: 'Guau Guau'},
+                {value: 'gato', text: 'Miau Miau'},
+                {value: 'pato', text: 'Kwak Kwak'},
+            ],
+            animalesRadio:[
+                {value: 'perro', text: 'Guau Guau'},
+                {value: 'gato', text: 'Miau Miau'},
+                {value: 'pato', text: 'Kwak Kwak'},
+                {value: 'cerdo', text: 'Oing Oing'},
+            ],
+            selected: [],
         }
     },
     computed:{
         comprobar(){
-
             return this.texto.length < 3 ? false : true;
         }
     }
