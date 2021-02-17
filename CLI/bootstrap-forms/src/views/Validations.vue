33c70d8040b99ac6fed2d6d4be24f00baa286c8c
TremalJ <>
2021-02-17 10:01:12 +0100

    Vuelidate, validador de formularios, validando contraseñas

--- CLI/bootstrap-forms/src/views/Validations.vue
+++ CLI/bootstrap-forms/src/views/Validations.vue
@@ -13,23 +13,51 @@
         >
         <p class="text-danger" v-if="!$v.email.email">El email no es correcto</p>
         <p class="text-danger" v-if="!$v.email.required">* Campo requerido</p>
-        <p>{{$v.email}}</p>
+        <!-- <p>{{$v.email}}</p> -->
+        <input type="password" 
+        placeholder="ingresa contraseña"  
+        v-model="$v.password.$model"
+        class="form-control m-2" 
+        :class="{'is-invalid': $v.password.$error}"
+        name="" id="">
+        <p class="text-danger" v-if="!$v.password.minLength">* Mínimo 6 caracteres</p>
+        <!-- <p>{{$v.password}}</p> -->
+        <input 
+        type="password" 
+        v-model="$v.repeatPassword.$model" 
+        placeholder="repite contraseña"  
+        class="form-control m-2" 
+        :class="{'is-invalid': $v.repeatPassword.$error}"
+        name="" 
+        id=""
+        >
+        <p class="text-danger" v-if="!$v.repeatPassword.sameAsPassword">* Las contraseñas no coinciden</p>
+        <!-- <p>{{$v.repeatPassword}}</p> -->
     </form>
     </div>
   
 </template>
 
 <script>
-import {required,email} from 'vuelidate/lib/validators'
+import {required,email, sameAs , minLength } from 'vuelidate/lib/validators'
 export default {
     name: 'Validations',
     data(){
         return{
-            email: ''
+            email: '',
+            password: '',
+            repeatPassword: '',
         }
     },
     validations:{
-        email:{ required, email}
+        email:{ required, email },
+        password:{ 
+            required, 
+            minLength: minLength(6) 
+        },
+        repeatPassword:{
+            sameAsPassword: sameAs('password')
+        } 
     }
 }
 </script>
