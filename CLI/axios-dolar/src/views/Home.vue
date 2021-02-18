5bf6e0033f551985f95fcbd29ea13c764e4e8fd4
TremalJ <>
2021-02-18 15:44:39 +0100

    axios-dolar.grid en vuetify

--- CLI/axios-dolar/src/views/Home.vue
+++ CLI/axios-dolar/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout :wrap="true">
+    <v-flex md6>
+      <h1>Hola mundo!</h1> 
+    </v-flex>
+    <v-flex md3>
+      <h1>Hola mundo!</h1> 
+    </v-flex>
+    <v-flex md3>
+      <h1>Hola mundo!</h1> 
+    </v-flex>
+    <v-flex md3>
+      <h1>Hola mundo!</h1> 
+    </v-flex>
+  </v-layout>  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
