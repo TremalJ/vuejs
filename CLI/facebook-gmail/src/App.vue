a4397da8303d7486bb7a172e0f42192395ba738b
TremalJ <>
2021-02-19 14:19:42 +0100

    facebook-gmail. Ingreso Gmail

--- CLI/facebook-gmail/src/App.vue
+++ CLI/facebook-gmail/src/App.vue
@@ -1,44 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-toolbar>
+      <v-toolbar-title class="headline text-uppercase" >
+       <span>Vue</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
+      <v-btn>
+        <span>Cerrar sesión</span>
+        <v-icon>fas fa-address-book</v-icon>
       </v-btn>
-    </v-app-bar>
-
+    </v-toolbar>
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
---
+++ CLI/facebook-gmail/src/views/Ingreso.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-layout justify-center mt-5>
+        <v-flex>
+            <v-card>
+                <v-card-text class="display-1 text-uppercase primary white--text text-center"
+                :class="registro ? 'success': 'primary'">
+                    <span class="text-center">{{ registro ? 'Registro' : 'Ingreso' }}</span>
+                </v-card-text>
+                <v-card-text block >
+                    <v-btn block color="error">
+                        <v-icon>fab fa-google</v-icon><span class="ml-4">Google</span>
+                    </v-btn>
+                    <v-btn block color="info">
+                        <v-icon>fab fa-facebook-f </v-icon><span class="ml-4">Facebook</span>
+                    </v-btn>
+                </v-card-text>
+                <v-card-text>
+                    <v-btn block @click="registro = !registro" v-if="!registro">
+                        ¿No tienes cuenta? Regístrate aquí
+                    </v-btn>
+                    <v-btn block @click="registro = !registro" v-if="registro">
+                        ¿Ya tienes cuenta? Ingresa aquí
+                    </v-btn>
+                </v-card-text>
+            </v-card>
+            
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            registro:true
+        }
+        
+    }
+}
+</script>
