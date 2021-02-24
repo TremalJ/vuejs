87e37c3df89e5ffdc60ed48d68a2dc6e3d54e456
TremalJ <>
2021-02-24 15:52:07 +0100

    nuxtJS.agregar CSS personalizados

--- CLI/nuxtjs/nuxt.config.js
+++ CLI/nuxtjs/nuxt.config.js
@@ -11,12 +11,14 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=ZCOOL+XiaoWei&display=swap' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
