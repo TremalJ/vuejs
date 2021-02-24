e6f27d31406f56a0e5418d3ff2ac7b3217bb528d
TremalJ <>
2021-02-24 14:15:29 +0100

    NuxtJS. Continue with NUXT.js

--- CLI/ejemplo-v2/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app" class="container">
-    <div id="nav" class="text-center m-3">
-      <router-link to="/"><i class="fas fa-home fa-3x"></i></router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
---
+++ pages/blog/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Titulo {{$route.params.id}} </h1>
+</template>
+
+<script>
+export default {
+  created () {
+  }
+}
+</script>
+
+<style>
+
+</style>
